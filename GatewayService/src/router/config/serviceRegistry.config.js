2d41dbd4a6b4b80b26712d1bc618614d6e1641e3
vaajiee <vajidmohammed94@gmail.com>
2023-04-23 17:46:48 +0530

    Added configuration file based routing

---
+++ GatewayService/src/router/config/serviceRegistry.config.js
@@ -0,0 +1,15 @@
+const services =  {
+        "auth": {
+            "apiName": "auth",
+            "host": "localhost",
+            "port": 5001,
+            "url": "http://localhost:5001/api/v1"
+        },
+        "post": {
+            "apiName": "post",
+            "host": "localhost",
+            "port": 5002,
+            "url": "http://localhost:5002/api/v1"
+        },
+    }
+export default services;
--- GatewayService/src/router/index.js
+++ GatewayService/src/router/index.js
@@ -1,17 +1,12 @@
 import { Router } from 'express';
 import axios from 'axios';
+import services from './config/serviceRegistry.config.js';
+
 const router = Router();
 
 router.all('/:appName', (req, res, next) => {
     const appName = req.params.appName;
-    let baseUrl = '';
-    console.log("App name is ", req.params.appName);
-    if (appName == "auth") {
-        baseUrl = "http://localhost:5001/api/v1/authservice"
-    }
-    else if (appName == "post") {
-        baseUrl = "http://localhost:5002/api/v1/postservice"
-    }
+    let baseUrl = `${services[appName].url}/${appName}`;
     axios.get(baseUrl).then((response) => {
         res.send(JSON.stringify(response.data));
     })
